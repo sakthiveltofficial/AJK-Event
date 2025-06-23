eb62c8dfb65a614786a0754b380f639cc6a426f8
sakthivel <sakthiveltofficial@gmail.com>
2025-06-23 11:59:41 +0530

    loader added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
+"use client";
 import CanvesWrapper from "@/Components/Common/CanvesWrapper";
 import Wall from "@/Three/Room/Wall";
 import { Idel } from "@/Three/Room/Scene";
+import { Loader } from "@react-three/drei";
 
 export default function Home() {
   return (
     <div className="w-full h-screen">
-
+      <Loader />
       <CanvesWrapper>
         <Idel />
         <Wall />
