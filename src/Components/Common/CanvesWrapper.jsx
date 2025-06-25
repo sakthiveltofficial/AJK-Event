20daa5eb9d9926843134e205b396bd9760eb024a
sakthivel <sakthiveltofficial@gmail.com>
2025-06-25 10:03:07 +0530

    first sequence is done

--- src/Components/Common/CanvesWrapper.jsx
+++ src/Components/Common/CanvesWrapper.jsx
@@ -6,26 +6,37 @@ import {
   Grid,
   OrbitControls,
 } from "@react-three/drei";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import BaseEnvironment from "./BaseEnvironment";
 import { getProject } from "@theatre/core";
-import { SheetProvider } from "@theatre/r3f";
+import { PerspectiveCamera, SheetProvider } from "@theatre/r3f";
 import extension from "@theatre/r3f/dist/extension";
 import studio from "@theatre/studio";
+import { editable as e } from "@theatre/r3f";
+import sequences from "@/../public/sequences/MainProject.theatre-project-state.json";
 
 // studio.initialize();
 // studio.extend(extension);
 
 function CanvesWrapper({ children }) {
-  const project = getProject("MainProject");
+  const project = getProject("MainProject", { state: sequences });
   const sheet = project.sheet("HeroScene");
+  const cameraLookAtRef = useRef(null);
 
+  useEffect(() => {
+    project.ready.then(() => {
+      setTimeout(() => {
+        const sheet = project.sheet("HeroScene");
+        sheet.sequence.play();
+      }, 4000);
+    });
+  }, []);
   return (
     <div className="w-full h-full relative p-5">
       <div className="w-full h-full relative bg-black rounded-[3rem] overflow-hidden">
         <Canvas
-          camera={{ fov: 70, position: [0, 2, 15] }}
-          gl={{ antialias: true }}
+          camera={{ fov: 70, position: [0, 2, 50] }}
+          gl={{ antialias: true, preserveDrawingBuffer: true }}
           dpr={[1, 1.5]}
           shadows
           style={{
@@ -37,6 +48,23 @@ function CanvesWrapper({ children }) {
           }}
         >
           <SheetProvider sheet={sheet}>
+            <PerspectiveCamera
+              makeDefault
+              position={[0, 2, 50]}
+              fov={70}
+              theatreKey="camera"
+              lookAt={cameraLookAtRef}
+            />
+            <e.mesh
+              theatreKey="camera_lookAt"
+              visible="editor"
+              position={[0, 2, 50]}
+              fov={70}
+              ref={cameraLookAtRef}
+            >
+              <octahedronGeometry args={[0.1, 0]} />
+              <meshStandardMaterial color="red" />
+            </e.mesh>
             <BaseEnvironment />
             {children}
             <OrbitControls />
