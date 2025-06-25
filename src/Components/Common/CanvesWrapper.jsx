c61fd288b9f616686d14f17103c556fe0b839b4e
sakthivel <sakthiveltofficial@gmail.com>
2025-06-25 06:29:56 +0530

    other pages

--- src/Components/Common/CanvesWrapper.jsx
+++ src/Components/Common/CanvesWrapper.jsx
@@ -8,8 +8,18 @@ import {
 } from "@react-three/drei";
 import React from "react";
 import BaseEnvironment from "./BaseEnvironment";
+import { getProject } from "@theatre/core";
+import { SheetProvider } from "@theatre/r3f";
+import extension from "@theatre/r3f/dist/extension";
+import studio from "@theatre/studio";
+
+// studio.initialize();
+// studio.extend(extension);
 
 function CanvesWrapper({ children }) {
+  const project = getProject("MainProject");
+  const sheet = project.sheet("HeroScene");
+
   return (
     <div className="w-full h-full relative p-5">
       <div className="w-full h-full relative bg-black rounded-[3rem] overflow-hidden">
@@ -26,24 +36,26 @@ function CanvesWrapper({ children }) {
             height: "100%",
           }}
         >
-          <BaseEnvironment />
-          {children}
-          <OrbitControls />
-          <ambientLight intensity={0.5} />
-          <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
-            <GizmoViewport
-              axisColors={["red", "green", "blue"]}
-              labelColor="black"
-            />
-          </GizmoHelper>
-          {/* <Grid
+          <SheetProvider sheet={sheet}>
+            <BaseEnvironment />
+            {children}
+            <OrbitControls />
+            <ambientLight intensity={0.5} />
+            <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+              <GizmoViewport
+                axisColors={["red", "green", "blue"]}
+                labelColor="black"
+              />
+            </GizmoHelper>
+            {/* <Grid
           position={[0, -0.65, 0]}
           args={[150, 200]}
           cellColor="black"
           cellSize={1}
           cellThickness={1}
-        /> */}
-        </Canvas> 
+          /> */}
+          </SheetProvider>
+        </Canvas>
       </div>
     </div>
   );
