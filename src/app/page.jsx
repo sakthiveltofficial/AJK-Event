d4f319249fdf654ade8a9a71759c703dbe0f7c78
sakthivel <sakthiveltofficial@gmail.com>
2025-07-01 17:03:31 +0530

    patch

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,8 @@
 import CanvesWrapper from "@/Components/Common/CanvesWrapper";
 import Wall from "@/Three/Room/Wall";
 import { Loader } from "@react-three/drei";
-import { RoboRoom  } from "@/Three/Robo/RoboRoom";
-import { Robo } from "@/Three/Robo";
+// import { RoboRoom  } from "@/Three/Robo/RoboRoom";
+// import { Robo } from "@/Three/Robo";
 import Idel from "@/Three/RoomWithRobo/Scene/Idel";
 import { Scientists } from "@/Three/Scientist/Scientist";
 // import { Model } from "@/Three/model";
